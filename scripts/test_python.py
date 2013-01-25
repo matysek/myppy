7cefacd7fd0e369d749df5a4325dc1ee7de97233
Martin Zibricky <mzibr.public@gmail.com>
2013-01-25 12:40:48 +0100

    Add script to test import of problematic Python modules.

---
+++ scripts/test_python.py
@@ -0,0 +1,10 @@
+# The following modules contain some binary C extensions that might cause
+# import errors if not properly compiled.
+
+# If you do not see any ImportError exceptions then they should work as expected.
+
+import curses
+import hashlib
+import readline
+import sqlite3
+import ssl
